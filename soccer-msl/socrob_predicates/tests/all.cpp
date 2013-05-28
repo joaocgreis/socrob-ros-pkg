2c2344eeb89a6849d2ce27c1c2c0c7f95c3d5606
João Reis <joaocgreis@gmail.com>
2013-05-28 15:49:12 +0100

    Simple test skeleton

---
+++ soccer-msl/socrob_predicates/tests/all.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+
+#include <socrob/predicates.h>
+
+#include "test_topic_generator.h"
+
+
+
+using namespace std;
+using namespace socrob::predicates;
+
+
+
+class PeriodicNegative :
+  public Predicate
+{
+    PeriodicNegative() {
+    }
+    
+    bool state() {
+    }
+};
+
+
+
+class PredicateTester :
+  private PredicateManager
+{
+  public:
+    PredicateTester() {
+      /*
+      RP peiodicNegative = add<PeriodicNegative>().name ("PeiodicNegative");
+      peiodicNegative->event_rise ("BallOurSide");
+      peiodicNegative->event_fall ("BallTheirSide");
+      */
+    }
+};
+
+
+
+int
+main (int argc,
+      char* argv[])
+{
+  ros::init (argc, argv, "predicates");
+  
+  TestTopicGenerator ttg;
+  
+  PredicateTester pt;
+  
+  ros::spin();
+  
+  return 0;
+}
