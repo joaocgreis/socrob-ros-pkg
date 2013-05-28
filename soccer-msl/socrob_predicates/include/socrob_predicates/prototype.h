3130fe30a9039ec2c471a31b1d5fb416e131a321
João Reis <joaocgreis@gmail.com>
2013-05-28 16:53:34 +0100

    Some more defenitions in the interface

--- soccer-msl/socrob_predicates/include/socrob_predicates/prototype.h
+++ soccer-msl/socrob_predicates/include/socrob_predicates/prototype.h
@@ -7,9 +7,33 @@ namespace socrob
 {
   namespace predicates
   {
-    class Predicate {};
-    class PredicateManager {};
+    class Predicate;
+    class PredicateManager;
+    class RP;
+    
+    class Predicate
+    {
+    };
+    
+    class PredicateManager
+    {
+    };
+    
+    class RP
+    {
+      public:
+        void
+        name (std::string const& name);
+    };
   }
+}
+
+
+
+inline void
+socrob::predicates::RP::
+name (const std::string& name)
+{
 }
 
 #endif
