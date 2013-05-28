84b9cd458bdae587ccaea3b26d95a506af9bc5c0
João Reis <joaocgreis@gmail.com>
2013-05-28 15:47:45 +0100

    Prototype skeleton

---
+++ soccer-msl/socrob_predicates/include/socrob/predicates.h
@@ -0,0 +1,6 @@
+#ifndef _SOCROB_PREDICATES_H_
+#define _SOCROB_PREDICATES_H_
+
+#include <socrob_predicates/prototype.h>
+
+#endif
---
+++ soccer-msl/socrob_predicates/include/socrob_predicates/prototype.h
@@ -0,0 +1,15 @@
+#ifndef SOCROB_PREDICATES_PROTOTYPE_H
+#define SOCROB_PREDICATES_PROTOTYPE_H
+
+
+
+namespace socrob
+{
+  namespace predicates
+  {
+    class Predicate {};
+    class PredicateManager {};
+  }
+}
+
+#endif
